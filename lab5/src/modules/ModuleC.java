06888d35f749f7dacd0d126960bc315786facfae
Thomas Lorincz <tlorincz@ualberta.ca>
2018-11-27 12:55:34 -0700

    Fixed Module C

--- lab5/src/modules/ModuleC.java
+++ lab5/src/modules/ModuleC.java
@@ -1,6 +1,8 @@
 package modules;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 import data.Entry;
 
 public class ModuleC {
@@ -13,23 +15,7 @@ public class ModuleC {
 
 	
 	public ArrayList<Entry> sortData(ArrayList<Entry> data) {
-		int increment = data.size() / 2;
-		while (increment > 0) {
-			for (int i = increment; i < data.size(); i++) {
-				int j = i;
-				Entry temp = data.get(i);
-				while (j > increment && data.get(j - increment).compareTo(temp) > 0) {
-					data.set(j, data.get(j-increment));
-					j = j - increment;
-				}
-				data.set(j, temp);
-			}
-			if (increment == 2) {
-				increment = 1;
-			} else {
-				increment *= (5.0 / 11);
-			}
-		}
+		Collections.sort(data);
 		f.displayData(data);
 		return data;
 	}
